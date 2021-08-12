11cf05aed385035a2b0203021c32d7dbadcf2dae
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-12 16:10:12 -0700

    added final  test

--- src/CodeChallange2/array.sort.test.js
+++ src/CodeChallange2/array.sort.test.js
@@ -1,4 +1,4 @@
-import { leastToGreatest, greatestToLeast, lengthSort, alphabetical } from './array-sort.js'
+import { leastToGreatest, greatestToLeast, lengthSort, alphabetical, byAge } from './array-sort.js'
 
 test('Least to greatest', () => {
     const input = [1, 3, 5, 2, 90, 20]; 
@@ -7,21 +7,35 @@ test('Least to greatest', () => {
   });
 
   test('Greatest to least', () => {
-    const input = [1, 3, 5, 2, 90, 20]; // arrange
-    const output = greatestToLeast(input); // act
-    expect(output).toEqual([90, 20, 5, 3, 2, 1]); // assert
+    const input = [1, 3, 5, 2, 90, 20]; 
+    const output = greatestToLeast(input); 
+    expect(output).toEqual([90, 20, 5, 3, 2, 1]); 
   });
 
   test('shortest word to longest', () => {
-    const input = ['dog', 'wolf', 'by', 'family', 'eaten']	; // arrange
-    const output = lengthSort(input); // act
-    expect(output).toEqual(['by', 'dog', 'wolf', 'eaten', 'family']); // assert
+    const input = ['dog', 'wolf', 'by', 'family', 'eaten']	; 
+    const output = lengthSort(input); 
+    expect(output).toEqual(['by', 'dog', 'wolf', 'eaten', 'family']); 
   });
 
   test('alphabetical sort', () => {
-    const input = ['dog', 'wolf', 'by', 'family', 'eaten']	; // arrange
-    const output = alphabetical(input); // act
-    expect(output).toEqual(['by', 'dog', 'eaten', 'family', 'wolf']); // assert
+    const input = ['dog', 'wolf', 'by', 'family', 'eaten']	; 
+    const output = alphabetical(input); 
+    expect(output).toEqual(['by', 'dog', 'eaten', 'family', 'wolf']); 
   });
   
-  
+  test('by age sort', () => {
+    const input = [
+        { name: 'Quiet Samurai', age: 22 },
+        { name: 'Arrogant Ambassador', age: 100 },
+        { name: 'Misunderstood Observer', age: 2 },
+        { name: 'Unlucky Swami', age: 77 }
+    ]; 
+  const output = byAge(input); 
+    expect(output).toEqual([ 
+        { name: 'Misunderstood Observer', age: 2 },
+        { name: 'Quiet Samurai', age: 22 },
+        { name: 'Unlucky Swami', age: 77 },
+        { name: 'Arrogant Ambassador', age: 100 } 
+    ]); 
+});
