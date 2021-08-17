8a6c338936aaca7196cad4be6334e2e005eb5e22
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-16 18:45:33 -0700

    passed all tests

--- src/CodeChallange3/objectKeyEntries.test.js
+++ src/CodeChallange3/objectKeyEntries.test.js
@@ -1,4 +1,4 @@
-import { capitalizeObjectKeys, sortedKeys } from '../CodeChallange3/objectKeyEntries.js';
+import { capitalizeObjectKeys, sortedKeys, getFilteredKey, getArrayOfKeysAndValues, sortedArraysByValuesLength } from '../CodeChallange3/objectKeyEntries.js';
 
 test('it capitalizes the keys', () => {
   const dataObject = { name: 'Angelina Jolie', age: 80 };
@@ -7,8 +7,26 @@ test('it capitalizes the keys', () => {
 
 });
 
-test('it sorts the keys by shortest to longest', () => {
-    const dataObject = { name: 'Spot', age: 3, isDog: true, friends: ['Rover', 'Steve', 'Fluffy'] }; //arrange
-    const output = sortedKeys(dataObject); //act
-    expect(output).toEqual(['age', 'name', 'isDog', 'friends']); //assert
+test('sorts keys by shortest to longest', () => {
+    const dataObject = { name: 'Spot', age: 3, isDog: true, friends: ['Rover', 'Steve', 'Fluffy'] }; 
+    const output = sortedKeys(dataObject); 
+    expect(output).toEqual(['age', 'name', 'isDog', 'friends']); 
+  });
+
+test('to see if function filters out age key', () => {
+    const dataObject = { name: 'Angelina Jolie', isSpot: false, age: 80, };
+  const output = getFilteredKey(dataObject); 
+  expect(output).toEqual(['age']); 
+});
+
+test('take an object and return a new array of arrays key vaule pairs', () => {
+    const dataObject = { name: 'Angelina Jolie', age: 80 }; //arrange
+    const output = getArrayOfKeysAndValues(dataObject); //act
+    expect(output).toEqual([['name', 'Angelina Jolie'], ['age', 80]]); //assert
+  });
+
+  test('to give back an array of arrays of key value pairs sorted longest to shortest by values length', () => {
+    const dataObject = { name: 'Bob', friend: 'Tom Hanks', location: 'Los Angeles' }; //arrange
+    const output = sortedArraysByValuesLength(dataObject); //act
+    expect(output).toEqual([['location', 'Los Angeles'], ['friend', 'Tom Hanks'], ['name', 'Bob']]); //assert
   });
