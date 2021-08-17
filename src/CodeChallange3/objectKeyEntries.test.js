ec88b1a8b48badf0487275f7346c80a68a3d076d
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-16 18:21:21 -0700

    passed first test

---
+++ src/CodeChallange3/objectKeyEntries.test.js
@@ -0,0 +1,7 @@
+import { capitalizeObjectKeys } from '../CodeChallange3/objectKeyEntries.js';
+
+test('it capitalizes the keys', () => {
+  const dataObject = { name: 'Angelina Jolie', age: 80 }; //arrange
+  const output = capitalizeObjectKeys(dataObject); //act
+  expect(output).toEqual(['NAME', 'AGE']); //assert
+});
