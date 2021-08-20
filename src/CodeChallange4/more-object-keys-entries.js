584aabcdbfd125a27ca7e8619b29dbac4a693cf9
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-19 19:10:59 -0700

    passed third test

--- src/CodeChallange4/more-object-keys-entries.js
+++ src/CodeChallange4/more-object-keys-entries.js
@@ -5,3 +5,17 @@ export function getHouses(obj) {
 export function updateNumbers(obj) {
     return Object.entries(obj).map(obj => obj[0] + ': ' + obj[1])
 }
+
+export function totalCharacters(arr) {
+    let total = 0;
+    Object.values(arr).map(arr => {
+        total += arr.children.length;
+        if (arr.spouse) total++;
+        return total ++;
+    });
+    return total;
+}
+
+export function hasChildEntries(arr, character) {
+    
+}
