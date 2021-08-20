78dd5426094752f61ce2217dc3c9ac927290a03d
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-19 20:37:55 -0700

    could not figure out the 3rd and 4th test

--- src/CodeChallange5/array-methods.test.js
+++ src/CodeChallange5/array-methods.test.js
@@ -1,4 +1,4 @@
-import { howMuchPencil, wordsToCharList } from './array-methods';
+import { howMuchPencil, wordsToCharList, listFoods, stepActions } from './array-methods';
 
  test('It should return a list of shortening words', () => {
     expect(howMuchPencil('Welcome')).toStrictEqual(['Welcome', 'elcome', 'lcome', 'come', 'ome', 'me', 'e', '']);
@@ -13,3 +13,66 @@ import { howMuchPencil, wordsToCharList } from './array-methods';
     expect(wordsToCharList('hooray')).toStrictEqual(['h', 'o', 'o', 'r', 'a', 'y']);
     expect(wordsToCharList('')).toStrictEqual([]);
   });
+
+  test('returns shopping list', () => {
+      const gruffaloCrumble = {
+          name: 'How to make a Gruffalo Crumble',
+          ingredients: [
+            '1 medium-sized Gruffalo',
+            '8 pounds oats',
+            '2 pounds brown sugar',
+            '4 pounds flour',
+            '2 gallons pure maple syrup',
+            '16 cups chopped nuts',
+            '1 pound baking soda',
+            '1 pound baking powder',
+            '1 pound cinnamon',
+            '6 gallons melted butter',
+            '2 gallons fresh water',
+        ],
+        steps: [
+            'Pre-heat a large oven to 375',
+            'De-prickle the gruffalo',
+            'Sprinkle with cinnamon, sugar, flour, and nuts',
+            'Mix until evenly distributed',
+            'Grease a 3-foot x 3-foot casserole dish',
+            'Combine gruffalo compote with water to maintain moisture in the oven',
+            'Fold together remaining ingredients to make the crisp',
+            'Spread the crisp evenly over the gruffalo mixture',
+            'Bake for 12-15 hours',
+        ]
+    }; 
+        expect(listFoods(gruffaloCrumble)).toStrictEqual(['Gruffalo', 'oats', 'brown sugar', 'flour', 'pure maple syrup', 'chopped nuts', 'baking soda', 'baking powder', 'cinnamon', 'melted butter', 'fresh water']);
+      });
+          
+      test('returns steps', () => {
+        const gruffaloCrumble = {
+            name: 'How to make a Gruffalo Crumble',
+            ingredients: [
+              '1 medium-sized Gruffalo',
+              '8 pounds oats',
+              '2 pounds brown sugar',
+              '4 pounds flour',
+              '2 gallons pure maple syrup',
+              '16 cups chopped nuts',
+              '1 pound baking soda',
+              '1 pound baking powder',
+              '1 pound cinnamon',
+              '6 gallons melted butter',
+              '2 gallons fresh water',
+          ],
+          steps: [
+              'Pre-heat a large oven to 375',
+              'De-prickle the gruffalo',
+              'Sprinkle with cinnamon, sugar, flour, and nuts',
+              'Mix until evenly distributed',
+              'Grease a 3-foot x 3-foot casserole dish',
+              'Combine gruffalo compote with water to maintain moisture in the oven',
+              'Fold together remaining ingredients to make the crisp',
+              'Spread the crisp evenly over the gruffalo mixture',
+              'Bake for 12-15 hours',
+          ]
+      }; 
+          expect(stepActions(gruffaloCrumble)).toStrictEqual(['Pre-heat', 'De-prickle', 'Sprinkle', 'Mix', 'Grease', 'Combine', 'Fold', 'Spread', 'Bake']
+          );
+        });
